91f52f8ed77de7edd114a722de3b1c38f50c095a
Reginaldo Lopes <103508426+regilopes@users.noreply.github.com>
2025-04-01 11:30:38 -0300

    Hotfix MGS3 slowdown and new area glitches
    Changed aim and cam to have dedicated pointers now fixing the slowdown when changing too fast from aiming to external cam, vice versa.
    
    Added state verification when loading a new area to fix the green screen glitch when changing areas.

--- games/ps2_mgs3.c
+++ games/ps2_mgs3.c
@@ -23,21 +23,33 @@
 #include "../mouse.h"
 #include "game.h"
 
+//TODO:
+//fix glitch when entering a new map (gameState loading)
+
 //cam and aim pointer (viewBase)
-#define MGS3_VIEWBASE 0x0020CF18
+//#define MGS3_VIEWBASE 0x0020CF18
+#define MGS3_CAMBASE 0x0020CEE0
+#define MGS3_AIMBASE 0x0024EEF8
+// #define MGS3_AIMBASE 0x0020CF18
+
 
 //States
 #define MGS3_VIEWSTATE 0x001D7463 //aiming or not aiming
+#define MGS3_GAMESTATE 0x0024EC8C //loading new area or not
 
 //cam offsets from viewbase
-#define MGS3_CAMY -0x100
-#define MGS3_CAMX -0x108
+#define MGS3_CAMY -0xC0
+#define MGS3_CAMX -0xC8
 
 //aim offsets from viewbase
-#define MGS3_AIMY 0x2E0
-#define MGS3_AIMX 0x2E2
+#define MGS3_AIMY 0xC670
+#define MGS3_AIMX 0xC672
+// #define MGS3_AIMY 0x2E0
+// #define MGS3_AIMX 0x2E2
 
-#define MGS3_FOV -0x14E84
+// #define MGS3_FOV -0x18D64
+//#define MGS3_FOV -0x14E84
+#define MGS3_FOV -0x8AF4
 
 
 static uint8_t PS2_MGS3_Status(void);
@@ -54,8 +66,10 @@ static const GAMEDRIVER GAMEDRIVER_INTERFACE =
 
 const GAMEDRIVER *GAME_PS2_MGS3 = &GAMEDRIVER_INTERFACE;
 
-static uint32_t viewBase = 0;
+static uint32_t camBase = 0;
+static uint32_t aimBase = 0;
 static uint8_t viewState = 0;
+static uint16_t gameState = 0;
 static float xAccumulator = 0.f;
 static float yAccumulator = 0.f;
 
@@ -81,22 +95,29 @@ static uint8_t PS2_MGS3_Status(void)
 //==========================================================================
 static void PS2_MGS3_Inject(void)
 {
+	
 	if(xmouse == 0 && ymouse == 0) // if mouse is idle
 	 	return;
+	
+	gameState = PS2_MEM_ReadUInt16(MGS3_GAMESTATE);
+	if(gameState != 0x0080) //if game is loading new area
+		return;
 
-	viewBase = PS2_MEM_ReadPointer(MGS3_VIEWBASE);
+	camBase = PS2_MEM_ReadPointer(MGS3_CAMBASE);
+	aimBase = PS2_MEM_ReadPointer(MGS3_AIMBASE);
 	viewState = PS2_MEM_ReadUInt8(MGS3_VIEWSTATE);
+	
 
-	float fov = PS2_MEM_ReadFloat(viewBase + MGS3_FOV);
+	float fov = PS2_MEM_ReadFloat(aimBase + MGS3_FOV);
 	float looksensitivity = (float)sensitivity;
 
 	//while not aiming
-	float camY = PS2_MEM_ReadFloat(viewBase + MGS3_CAMY);
-	uint16_t camX = PS2_MEM_ReadUInt(viewBase + MGS3_CAMX);
+	float camY = PS2_MEM_ReadFloat(camBase + MGS3_CAMY);
+	uint16_t camX = PS2_MEM_ReadUInt(camBase + MGS3_CAMX);
 
 	//while aiming
-	uint16_t aimY = PS2_MEM_ReadUInt16(viewBase + MGS3_AIMY);
-	uint16_t aimX = PS2_MEM_ReadUInt16(viewBase + MGS3_AIMX);
+	uint16_t aimY = PS2_MEM_ReadUInt16(aimBase + MGS3_AIMY);
+	uint16_t aimX = PS2_MEM_ReadUInt16(aimBase + MGS3_AIMX);
 	float aimYf = (float)aimY;
 	float aimXf = (float)aimX;
 	
@@ -111,10 +132,10 @@ static void PS2_MGS3_Inject(void)
 
 
 	float ym = (float)(invertpitch ? -ymouse : ymouse);
-	float dy = ym * looksensitivity / fov;
+	float dy = ym * looksensitivity / fov / 2.f;
 	AccumulateAddRemainder(&aimYf, &yAccumulator, ym, dy);
 
-	float dx = (float)-xmouse * looksensitivity / fov;
+	float dx = (float)-xmouse * looksensitivity / fov / 2.f;
 	AccumulateAddRemainder(&aimXf, &xAccumulator, (float)xmouse, dx);
 
 	// aimY += (uint16_t)(!invertpitch ? ymouse : -ymouse) * looksensitivity / fov / 2;
@@ -122,13 +143,13 @@ static void PS2_MGS3_Inject(void)
 	//aimX = ClampInt(aimX, 0, 65536);
 	
 	if (viewState == 0x00){
-		PS2_MEM_WriteFloat(viewBase + MGS3_CAMY, camY);
-		PS2_MEM_WriteUInt16(viewBase + MGS3_CAMX, camX);
+		PS2_MEM_WriteFloat(camBase + MGS3_CAMY, camY);
+		PS2_MEM_WriteUInt16(camBase + MGS3_CAMX, camX);
 	}
 	
 	if (viewState == 0x04 ||// first person
 		viewState == 0x14){ //binoculars/scope
-		PS2_MEM_WriteUInt16(viewBase + MGS3_AIMY, (uint16_t)aimYf);
-		PS2_MEM_WriteUInt16(viewBase + MGS3_AIMX, (uint16_t)aimXf);
+		PS2_MEM_WriteUInt16(aimBase + MGS3_AIMY, (uint16_t)aimYf);
+		PS2_MEM_WriteUInt16(aimBase + MGS3_AIMX, (uint16_t)aimXf);
 	}
 }
