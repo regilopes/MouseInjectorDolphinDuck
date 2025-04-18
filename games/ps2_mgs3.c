ba00efbfef55ef411479d87cface3c4a41973d01
Reginaldo Lopes <103508426+regilopes@users.noreply.github.com>
2025-04-18 12:18:32 -0300

    MGS:3 code cleaning

--- games/ps2_mgs3.c
+++ games/ps2_mgs3.c
@@ -27,47 +27,29 @@
 // auto ready weapon when aiming
 
 // cam and aim pointer (viewBase)
-// #define MGS3_VIEWBASE 0x0020CF18
-// #define MGS3_CAMBASE 0x0020CEE0
 #define MGS3_CAMBASE 0x0020CF18 //(viewbase)
-// #define MGS3_AIMBASE 0x001C53A0 //aim base pointer 3.0
-// #define MGS3_AIMBASE 0x0024edf0 //aim base pointer 2.0
-// #define MGS3_AIMBASE 0x0024EEF8  //aim
 #define MGS3_AIMBASE 0x0020CF18	   // aim base (viewbase)
 #define MGS3_CUTSCNBASE 0x00213670 // cutscene cam base
 
 // States
 #define MGS3_FPCAMSTATE 0x001D744C // 2.0 FPV or TPV
 #define MGS3_VIEWSTATE 0x0020CF9F  // FPV or TPV
-// #define MGS3_VIEWSTATE 0x001D7463 //FPV or TPV
 #define MGS3_GAMESTATE 0x0024EC8B // loading new area or not
 #define MGS3_CUTSCNSTATE 0x50	  // is FP on cutscene or not (offset from cutscnebase)
 #define MGS3_WEPSTATE 0x1E8A	  // wepon ready, reloading, empty... (offset from aimbase)
 
 // cam offsets from cambase
-// #define MGS3_CAMY -0xC0
-// #define MGS3_CAMX -0xC8
 #define MGS3_CAMY -0x100
 #define MGS3_CAMX -0x108
 
 // aim offsets from viewbase
-// #define MGS3_AIMY 0xFFFFCF30 //aim offset 3.0
-// #define MGS3_AIMX 0xFFFFCF32 //aim offset 3.0
-//  #define MGS3_AIMY 0xA3D0 //aim offset 2.0
-//  #define MGS3_AIMX 0xA3D2 //aim offset 2.0
-// #define MGS3_AIMY 0xC670 //aim
-// #define MGS3_AIMX 0xC672 //aim
 #define MGS3_AIMY 0x2E0 // aim base
 #define MGS3_AIMX 0x2E2 // aim base
 
 #define MGS3_CUTSCNY 0x5C
 #define MGS3_CUTSCNX 0x60
 
-// #define MGS3_FOV -0x18D64
-// #define MGS3_FOV -0x14E84 //day
-// #define MGS3_FOV -0x14F44 //night
-// #define MGS3_FOV -0x8AF4  //day
-// #define MGS3_FOV -0x8BB4  //night
+
 #define MGS3_FOV 0x0020CF9C
 
 static uint8_t PS2_MGS3_Status(void);
@@ -125,7 +107,7 @@ static void PS2_MGS3_Inject(void)
 	cutscnState = PS2_MEM_ReadUInt8(cutscnBase + MGS3_CUTSCNSTATE);
 	fpcamState = PS2_MEM_ReadUInt8(MGS3_FPCAMSTATE);
 
-	// float fov = PS2_MEM_ReadFloat(aimBase + MGS3_FOV);
+
 	float fov = PS2_MEM_ReadFloat(MGS3_FOV);
 	float looksensitivity = (float)sensitivity;
 
